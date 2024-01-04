398854a8d29e4ee779900901450c6e0731e99fb8
ordinarygirl4 <155393648+ordinarygirl4@users.noreply.github.com>
2024-01-03 21:39:57 -0500

    Update DemoB.java

--- src/b/DemoB.java
+++ src/b/DemoB.java
@@ -4,4 +4,7 @@ public class DemoB {
 public static void main(String[] args) {
 	System.out.println("Jill");
 }
+	//pull from github
+	public static void add(int a, int b) {
+	System.out.println(a+b);
 }
